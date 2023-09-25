a466170f01869b4f1bb624b394475107702e387b
Nameless37 <86179689+Nameless37@users.noreply.github.com>
2023-09-25 12:09:55 +0530

    Update Training.py

--- Training.py
+++ Training.py
@@ -1,4 +1,11 @@
-def fun():
-    print("abc")
+import math
 
-print("dsdasf")    
+class Circle:
+  def __init__(self,r):
+    self.r = r
+
+  def area(self):
+    return math.pi * (self.r ** 2)
+
+  def circumference(self):
+    return 2 * math.pi * self.r
