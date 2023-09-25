c4cc00b84215f1c6b7391e206a8fdee988165a83
Nameless37 <86179689+Nameless37@users.noreply.github.com>
2023-09-25 12:15:58 +0530

    Update Training.py

--- Training.py
+++ Training.py
@@ -1,11 +1,11 @@
 import math
 
-class Circle:
+class circle:
   def __init__(self,r):
     self.r = r
 
   def area(self):
-    return math.pi * (self.r ** 2)
+    return math.pi*(self.r ** 2)
 
-  def circumference(self):
+  def circum(self):
     return 2 * math.pi * self.r
